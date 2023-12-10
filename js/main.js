dd328bbb9935156bd34a9f99001752af23db7a7c
Mazen Salah <101013388+MazenSalahb@users.noreply.github.com>
2023-12-10 16:55:26 +0200

    adding post & register with image nctionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 setupUI();
 
-
 //----------------------- Handle Login & Register & Logout -----------------------
 function loginBtnClick() {
   const username = document.getElementById("username-input").value;
@@ -27,31 +26,34 @@ function loginBtnClick() {
     });
 }
 
-function registerBtnClick(){
+function registerBtnClick() {
   const username = document.getElementById("reg-username-input").value;
   const password = document.getElementById("reg-password-input").value;
   const name = document.getElementById("reg-name-input").value;
   const email = document.getElementById("reg-email-input").value;
-  axios.post(`${baseUrl}/register`,{
-    username,
-    password,
-    name,
-    email
-  })
-  .then((res)=>{
-    localStorage.setItem("token", res.data.token);
-    localStorage.setItem("user", JSON.stringify(res.data.user));
+  const image = document.getElementById("reg-image-input").files[0];
+  let formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", password);
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("image", image);
+  axios
+    .post(`${baseUrl}/register`, formData)
+    .then((res) => {
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
 
-    bootstrap.Modal.getInstance(
-      document.getElementById("register-modal")
-    ).hide();
-    // Show Success Toast
-    showToast("Congratulations! You have successfully registered.");
-    setupUI();
-  })
-  .catch((err)=>{
-    alert(err.response.data.message)
-  })
+      bootstrap.Modal.getInstance(
+        document.getElementById("register-modal")
+      ).hide();
+      // Show Success Toast
+      showToast("Congratulations! You have successfully registered.");
+      setupUI();
+    })
+    .catch((err) => {
+      alert(err.response.data.message);
+    });
 }
 
 // Handle Logout
@@ -76,19 +78,21 @@ function setupUI() {
   const loginBtn = document.getElementById("login-btn");
   const registerBtn = document.getElementById("register-btn");
   const logoutBtn = document.getElementById("logout-btn");
+  const addPostBtn = document.getElementById("add-btn");
   if (Auth()) {
     loginBtn.style.display = "none";
     registerBtn.style.display = "none";
     logoutBtn.style.display = "inline";
+    addPostBtn.style.display = "flex";
   } else {
     logoutBtn.style.display = "none";
     loginBtn.style.display = "inline";
     registerBtn.style.display = "inline";
+    addPostBtn.style.display = "none";
   }
 }
 
-
-function showToast(message){
+function showToast(message) {
   document.getElementById("toast-body").innerHTML = message;
   const toastLiveExample = document.getElementById("liveToast");
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
